a92a606218cbf9199ea0c4bb03dce9dff9b151c4
Anshul Tamrakar <anshultamrakar487@gmail.com>
2023-03-06 18:04:39 +0530

    more question on FP

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -380,14 +380,19 @@
 
 // Reduce Function -------------------------------------------------------------------------------------------------------------------------
 
-const array = [1,2,3,4,5,6,7,8]
+// const array = [1,2,3,4,5,6,7,8]
 
 
 // Given the Array of numbers return an object for each items
 
 
 
+
+
 // Given the array of numbers calculate the sum
+
+// const sumArray = array.reduce((acc , currValue) => acc + currValue)
+// console.log(sumArray)
 
 // const getSum = array.reduce((acc , currVal) => acc + currVal)
 // console.log(getSum)
@@ -415,6 +420,262 @@ const array = [1,2,3,4,5,6,7,8]
 
 // Return the array with string which have vowels
 // Return an array of objects with key as an items and value as numbers of character in string 
+
+
+
+
+
+
+
+// Practice Questions --- Neog questions
+// Given an array of numbers, write a function that returns the sum of all the even numbers.
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// // Output: 20
+
+// const sumOfEvenNum = (numbers) => numbers.reduce((acc , val) => val % 2 === 0 ? acc + val : acc, 0)
+// console.log(sumOfEvenNum(numbers))  
+
+
+
+
+// Given an array of strings, write a function that returns the total number of characters in all the strings that start with the letter "A".
+
+// const strings = ['Apple', 'Aardvark', 'Banana', 'Cat'];
+// Output: 13
+
+// const getTotalNumCharacter = (strings) => strings.reduce((acc , val) => val[0] === "A" ? (acc + val).length : acc) 
+
+// console.log(getTotalNumCharacter(strings))
+
+
+
+// Given an array of numbers, write a function that returns the sum of the cubes of all odd numbers in the array.
+// const numbers = [1, 2, 3, 4, 5];
+// Output: 153
+
+// const getCubesSum = (numbers) => numbers.reduce((acc , val) => val % 2 !== 0 ? acc + (val * val * val) : acc , 0)
+// console.log(getCubesSum(numbers))
+
+
+// Given an array. Write a function that takes in the given array and prints only the numbers which are less than 8 and also an even number.
+//  const arr = [2,4,5,7,9,17,34,65,90]
+// Output = [2, 4]
+
+// const getNewArr = (arr) => arr.filter(newArr => newArr < 8 && newArr % 2 === 0 )
+// console.log(getNewArr(arr))
+
+
+
+
+
+// Given an array of objects representing students, where each object has a name property and a grades property 
+// (an array of numbers), write a function that returns an array of objects representing the students who have an average grade of at least 90.
+
+// const students = [
+//   { name: 'Alice', grades: [90, 95, 92] },
+//   { name: 'Bob', grades: [80, 85, 87] },
+//   { name: 'Charlie', grades: [92, 88, 94] },
+//   { name: 'David', grades: [75, 80, 82] },
+// ];
+
+
+// Output: [{ name: 'Alice', averageGrade: 92.33333333333333 }, { name: 'Charlie', averageGrade: 91.33333333333333 }]
+
+
+// const calculateAvg = grades => grades.reduce((acc , val) => acc + val , 0)/grades.length
+
+// const findStudentAvg = students => students.map(({name , grades}) => ({name , averageGrade : calculateAvg(grades)}))
+// .filter(({averageGrade}) => averageGrade > 90)
+
+// console.log(findStudentAvg(students))       
+
+
+
+
+
+
+// Functional Programming Set 3 --------------------------------------------------------------------------------------------------------
+
+// 1. Write an ES6 function that takes an array of objects representing cars with properties make, model, and year.
+// Return the first car object that is a Toyota and the year is after 2010.
+
+// const cars = [
+//   { make: "Toyota", model: "Corolla", year: 2010 },
+//   { make: "Honda", model: "Civic", year: 2012 },
+//   { make: "Toyota", model: "Camry", year: 2015 },
+//   { make: "Ford", model: "Mustang", year: 2018 },
+// ];
+
+// const findToyotaCar = (cars) => cars.filter(({make , year}) => make === "Toyota" && year > 2010)
+// console.log(findToyotaCar(cars))
+
+
+
+// 2.Write an ES6 function that takes an array of objects containing car information (make, model, year) 
+// and returns an array with only the cars that were made after the year 2012
+
+// const cars = [
+//   { make: "Toyota", model: "Corolla", year: 2010 },
+//   { make: "Honda", model: "Civic", year: 2012 },
+//   { make: "Toyota", model: "Camry", year: 2015 },
+//   { make: "Ford", model: "Mustang", year: 2018 },
+// ];
+
+// const getCarDetails = cars => cars.filter(car => car.year > 2012)
+// const getCars = getCarDetails(cars)
+// console.log(getCars)
+
+// 3.Write an ES6 function that takes an array of objects representing products with properties name, price, and category. 
+// Return the first product object that is in the category "electronics".
+
+// const products = [
+//   { name: "Toothbrush", price: 29, category: "health" },
+//   { name: "Coffee Maker", price: 99, category: "home" },
+// 	{ name: "iPad", price: 799, category: "electronics" },
+//   { name: "Smartwatch", price: 199, category: "electronics" },
+// ];
+
+
+// const findElectronicsProduct = products => products.find(product => product.category === "electronics")
+
+// const electronicsProduct = findElectronicsProduct(products)
+
+// console.log(electronicsProduct)
+
+
+// 4 .Write an ES6 function that takes an array of objects representing products with properties name, price and category. 
+// Return all the products object that are in the category "electronics".
+
+// const products = [
+//   { name: "Toothbrush", price: 29, category: "health" },
+//   { name: "Coffee Maker", price: 99, category: "home" },
+// 	{ name: "iPad", price: 799, category: "electronics" },
+//   { name: "Smartwatch", price: 199, category: "electronics" },
+// ];
+
+// const getAllElectronicsProducts = products => products.filter(product => product.category === "electronics")
+// const electronicProducts = getAllElectronicsProducts(products)
+// console.log(electronicProducts)
+
+
+// 5. Write an ES6 function that takes an array of objects containing student information (name, age, grade) and 
+// returns an array with only the students who have a grade above a certain value.
+
+// const students = [
+//   { name: "Alice", age: 16, grade: 90 },
+//   { name: "Bob", age: 17, grade: 80 },
+//   { name: "Charlie", age: 15, grade: 95 },
+//   { name: "David", age: 16, grade: 85 },
+// ];
+
+
+// const filterStudentByGrade = (students, grade) =>  students.filter(student => student.grade >  grade)
+
+// const highGradeStudents = filterStudentByGrade(students , 85)
+// console.log(highGradeStudents)
+
+
+// 6.Write an ES6 function that takes an array of objects representing books with properties title, author and pageCount.
+//  Return the first book object that has more than 500 pages.
+
+// const books = [
+//   { title: "The Lord of the Rings", author: "J.R.R. Tolkien", pageCount: 1178 },
+//   { title: "To Kill a Mockingbird", author: "Harper Lee", pageCount: 281 },
+//   { title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", pageCount: 193 },
+//   { title: "The Name of the Wind", author: "Patrick Rothfuss", pageCount: 662 },
+// ];
+
+// const findBookWithMoreThan500Pages = books => books.find(book => book.pageCount > 500)
+
+// const bookWithMoreThan500Pages  = findBookWithMoreThan500Pages(books)
+// console.log(bookWithMoreThan500Pages)
+
+
+// 7.Write an ES6 function that takes an array of objects containing customer information (name, age, gender) and 
+// returns an array with only the customers who are of gender Male.
+
+// const customers = [
+//   { name: 'John', age: 25, gender: 'Male' },
+//   { name: 'Jane', age: 30, gender: 'Female' },
+//   { name: 'Bob', age: 40, gender: 'Male' },
+//   { name: 'Alice', age: 35, gender: 'Female' },
+// ];
+
+// const getMaleCustomer = (customers) => customers.filter(customer => customer.gender === "Male")
+// const maleCustomers = getMaleCustomer(customers)
+// console.log(maleCustomers)
+
+
+// 8.Write an ES6 function that takes an array of objects containing game information (title, developer, genre) and 
+// returns an array with all the games of a certain genre.
+
+// const games = [
+//   {title: "The Witcher 3: Wild Hunt", developer: "CD Projekt Red", genre: "RPG"},
+//   {title: "Grand Theft Auto V", developer: "Rockstar North", genre: "Action"},
+//   {title: "Portal 2", developer: "Valve Corporation", genre: "Puzzle"},
+//   {title: "The Legend of Zelda: Breath of the Wild", developer: "Nintendo", genre: "Adventure"}
+// ];
+
+// const filterByGenre = (games, genre)  => games.filter(game => game.genre === genre)
+// const filteredGames = filterByGenre(games, "RPG");
+// console.log(filteredGames)
+
+// 9.Write an ES6 function that takes an array of objects containing car information (make, model, year) and 
+// returns an array with only the car model that were made after the year 2012.
+
+
+// const cars = [
+//   { make: "Toyota", model: "Corolla", year: 2010 },
+//   { make: "Honda", model: "Civic", year: 2012 },
+//   { make: "Toyota", model: "Camry", year: 2015 },
+//   { make: "Ford", model: "Mustang", year: 2018 },
+// ];
+
+// const getCarModal = cars => cars.filter(car => car.year > 2012).map(car => car.model)
+
+// const carModals = getCarModal(cars)
+
+// console.log(carModals)
+
+
+// 10.Write an ES6 function that takes an array of objects representing books with properties title, author and pageCount. 
+// Return the all book titles that have more than 700 pages.
+
+// const books = [
+//   { title: "The Lord of the Rings", author: "J.R.R. Tolkien", pageCount: 1178 },
+//   { title: "To Kill a Mockingbird", author: "Harper Lee", pageCount: 281 },
+//   { title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", pageCount: 193 },
+//   { title: "The Name of the Wind", author: "Patrick Rothfuss", pageCount: 662 },
+// ];
+
+
+// const getTitlesWithMoreThan500Pages = (books) => books.filter(book => book.pageCount > 700).map(book => book.title)
+// const booksWithMoreThan500Pages = getTitlesWithMoreThan500Pages(books)
+// console.log(booksWithMoreThan500Pages)
+
+
+// 11.Write an ES6 function that takes an array of numbers and returns the sum of all the even numbers in the array using 
+// the reduce function.
+
+// const numbers = [12, 23, 4, 2, 11, 21] 
+
+// const sumOfEvenNumbers = numbers => numbers.reduce((acc , value) => value % 2 === 0 ? acc + value : acc)
+
+// console.log(sumOfEvenNumbers(numbers))
+
+
+// 12.Write an ES6 function that takes an array of objects representing students with properties name and score, and 
+// returns the average score of all the students using the reduce function.
+
+// const students = [
+//   { name: 'John', score: 80 },
+//   { name: 'Jane', score: 90 },
+//   { name: 'Bob', score: 75 },
+//   { name: 'Alice', score: 85 },
+// ];
+
+// const getAverageScore = students => students.map(student => student.score).reduce((acc , value) => acc + value)
+// console.log(getAverageScore(students))
 
 
 
