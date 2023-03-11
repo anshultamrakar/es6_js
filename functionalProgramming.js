652a2b6bde6102055420211e9b07444ad6b1c6e5
Anshul Tamrakar <anshultamrakar487@gmail.com>
2023-03-11 16:37:08 +0530

    added fp questions

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -804,11 +804,20 @@
 //     { name: "Slipper", price: 40, quantity: 3 }
 //   ];
 
+// const findMostExpensiveProduct = (products) => products.reduce((acc , value) => acc < value.price ? acc : value)
+// console.log(findMostExpensiveProduct(products))
+
+
+
 // 10. Write an ES6 function that takes an array of strings and returns an object with the count of each string.
 
 // const fruits = ['apple', 'banana', 'banana', 'cherry', 'apple', 'apple', 'banana'];
 
-// const countStrings = fruits => fruits.reduce({\})
+// const countStrings = fruits => fruits.reduce((acc , value) => (acc[value] = acc[value]+1 || 1 , acc), {})
+
+// console.log(countStrings(fruits))
+
+
 
 
 
